77581db0ac3673e90942f99c2b5dfd79a38dcc41
anikaislam16 <islamanika252@gmail.com>
2024-09-25 08:57:37 +0600

    Add donation section

--- js/features.js
+++ js/features.js
@@ -1,7 +1,6 @@
 function showSectionById(Id, selectedButton) {
     console.log('hi');
     
-  // Get the divs
   const section1 = document.getElementById("donation");
   const section2 = document.getElementById("history");
   section1.classList.add("hidden");
@@ -20,4 +19,47 @@ function showSectionById(Id, selectedButton) {
   
   selectedButton.classList.remove("bg-white", "border-2");
   selectedButton.classList.add("bg-[#B4F461]"); 
+}
+
+
+function handleDonation(
+  donationInputId,
+  donationSpanId,
+  totalSpanId
+) {
+  const donationInput = document.getElementById(donationInputId);
+  const donationSpan = document.getElementById(donationSpanId);
+  const totalSpan = document.getElementById(totalSpanId);
+  const donationAmount = parseFloat(donationInput.value);
+  const currentDonation = parseFloat(
+    donationSpan.innerText.replace(" BDT", "")
+  );
+  let totalMoney = parseFloat(totalSpan.innerText.replace(" BDT", ""));
+  const totalBalance = parseFloat(totalSpan.innerText.replace(" BDT", ""));
+
+  if (donationInput.value===""|| isNaN(donationInput.value) || donationAmount <= 0) {
+    alert("Please enter a valid donation amount.");
+    return;
+  }
+  if (donationAmount > totalBalance) {
+    alert("Insufficient balance to make this donation.");
+    return;
+  }
+  donationSpan.innerText = `${currentDonation + donationAmount} BDT`;
+  totalMoney -= donationAmount;
+  totalSpan.innerText = `${totalMoney} BDT`;
+  /// show modal using daisy ui
+  
+  const modal = document.getElementById("my_modal_4");
+  modal.showModal();
+
+
+  // // Add a notification to the history section
+  // const historyMessage = `You donated ${donationAmount} BDT. Current total is ${totalMoney} BDT.`;
+  // const historyEntry = document.createElement("p");
+  // historyEntry.innerText = historyMessage;
+  // historySection.appendChild(historyEntry);
+
+  // Clear the donation input
+  donationInput.value = "";
 }
